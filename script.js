75c8065a447acb0f1e21649911c8dff384590a78
tomzy-ba <trbee100@gmail.com>
2024-02-01 19:22:49 +0000

    added base js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function createUser (player1, player2) {
+    return {
+        player1: player1,
+        player2: player2,
+        
+        nameOutput: function() {
+            console.log(`Hi my name is ${this.player1} and this is ${this.player2}`)
+        }
+
+    }
+
+}
+const players = createUser("tom", "jerry");
+players.nameOutput();
