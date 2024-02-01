2197f5a70b02327861c00039068372953ac9b49b
tomzy-ba <trbee100@gmail.com>
2024-02-01 20:06:55 +0000

    new factory added

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 const gameBoard = [];
 
 function createUser (player1, player2) {
-    return {
-        player1: player1,
-        player2: player2,
+        player1 = player1;
+        player2 = player2;
         
-        nameOutput: function() {
-            console.log(`Hi my name is ${this.player1} and this is ${this.player2}`)
-        }
+        return {player1, player2}
 
-    }
 
 }
-const players = createUser("tom", "jerry");
-players.nameOutput();
+console.log(createUser("tom", "Jerry"))
+
+
+function makeMove (playerMove) {
+    playerMove = playerMove;
+    
+}
