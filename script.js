0b0c19491fa3ad3d419a345bdd1cc621ebc54d4b
tomzy-ba <trbee100@gmail.com>
2024-02-01 22:44:57 +0000

    fixed css problems

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // so first we need to create
-const gameBoard = [];
+const board = ["lol"];
+
+function gameBoard(topleft, topmid, topright, midleft, midmid, midright, bottomleft, bottommid, bottomright) {
+    topleft = topleft;
+    topmid = topmid;
+    topright = topright;
+    midleft = midleft;
+    midmid = midmid;
+    midright = midright;
+    bottomleft = bottomleft;
+    bottommid = bottommid;
+    bottomright = bottomright;
+
+    return {topleft, topmid, topright, midleft, midmid, midright, bottomleft, bottommid, bottomright };
+}
+console.log(gameBoard(board))
 
 function createUser (player1, player2) {
         player1 = player1;
@@ -9,10 +24,14 @@ function createUser (player1, player2) {
 
 
 }
-console.log(createUser("tom", "Jerry"))
 
 
-function makeMove (playerMove) {
-    playerMove = playerMove;
-    
+function makeMove (move, place) {
+    move = move;
+    place = place;
+
+    return {
+        
+    }
+
 }
