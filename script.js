d1025aa979cafb6a1c32d81b9047260216a9d074
tomzy-ba <trbee100@gmail.com>
2024-02-02 22:22:53 +0000

    dom elements added

--- script.js
+++ script.js
@@ -12,55 +12,59 @@ function startGame() {
     players(player1, player2);
     console.log(player1Name, player2Name);
 }
-startGame();
 
 
 const board = [
     1, 2, 3,
     4, 5, 6,
-    7, 8, 9];
+    7, 8, 9
+    ];
 
     function checkWin() {
         if (board[0] === board[1] && board[0] === board[2] ||
             board[3] === board[4] && board[3] === board[5] || 
             board[6] === board[7] && board[6] === board[8]) {
-            console.log("GAME OVER");
             game = false;
         }
     }
-console.log(board[0]);
 
 function playGame()  {
     game = true;
     while (game == true) {
-        checkWin();
         inputPlace = prompt("Player 1, enter the cell you want to play in: 1-9");
-        inputPlace = parseInt(inputPlace);
         board[inputPlace-1] = "X";
+        addDom();
 
         console.log(board);
         checkWin();
+        if (game == false) {console.log("Player1 Wins!"); break; }
 
-
+        
 
         inputPlace = prompt("Player 2, enter the cell you want to play in: 1-9");
-        inputPlace = parseInt(inputPlace);
         board[inputPlace-1] = "O";
+        addDom();
 
         console.log(board);
         checkWin();
+        if (game == false) {console.log("Player2 Wins!"); break; }
     }
 
     }
 
+    function addDom() {
+        const gameContainer = document.getElementById("game-board");
+        gameContainer.innerHTML = "";
+        board.forEach((cell, index) => {
+            const cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell");
+            cellDiv.innerHTML = cell;
+            gameContainer.appendChild(cellDiv);
+        });
 
+        }
 
-    
-    
-// how to get these values in the array
-
-
-
-
+console.log(board)
+startGame();
 playGame();
-// how to get a value of a cell in a 2D array
+addDom();
